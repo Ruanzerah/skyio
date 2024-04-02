256dd5c5355530d2bddda2f44eab8cf059b6c43e
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-02 20:40:24 -0300

    Controllers para registrar e logar na conta, lógica do loadUser mudada, linha "            SimpleGrantedAuthority roleUser = new SimpleGrantedAuthority("ROLE_USER"); " removida de SecurityFilter (não fazia mais sentido) e setado algumas url que podem ser acessadas por determinados cargos, por usuarios comuns e por quem não esta logado

--- src/main/java/com/ruanzerah/skyio/config/CustomUserDetailsService.java
+++ src/main/java/com/ruanzerah/skyio/config/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.ruanzerah.skyio.config;
 
-import com.ruanzerah.skyio.domain.user.User;
 import com.ruanzerah.skyio.domain.user.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,16 +7,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-
 @AllArgsConstructor
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        return userRepository.findByName(name).orElseThrow(() -> new UsernameNotFoundException("Algum erro desconhecido aconteceu, não foi possivel encontrar o seu usuario"));
     }
 }
