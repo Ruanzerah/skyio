c7bb62e761fe5ee2025eb3abbd967f5682bd6fc5
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-02 20:36:35 -0300

    cors não irá atender origens null

--- src/main/java/com/ruanzerah/skyio/config/CorsConfiguration.java
+++ src/main/java/com/ruanzerah/skyio/config/CorsConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200", "null").allowedMethods("GET", "POST");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST");
     }
 }
