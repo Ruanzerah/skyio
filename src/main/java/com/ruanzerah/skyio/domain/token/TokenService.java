1842389b695838d6cf3c15b530ffa93d318bee66
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-02 20:36:03 -0300

    O tempo do token agora é 2 horas e meia ao inves de 40 minutos

--- src/main/java/com/ruanzerah/skyio/domain/token/TokenService.java
+++ src/main/java/com/ruanzerah/skyio/domain/token/TokenService.java
@@ -1,4 +1,4 @@
-package com.ruanzerah.skyio.token;
+package com.ruanzerah.skyio.domain.token;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -33,6 +33,6 @@ public class TokenService {
     }
 
     private Instant expireTokenTime() {
-        return LocalDateTime.now().plusMinutes(40).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(2).plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
     }
 }
