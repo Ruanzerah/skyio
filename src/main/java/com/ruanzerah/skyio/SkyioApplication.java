e2a865c7bfa4715972674ee9ef74859b4c0d3258
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-07 22:32:47 -0300

    uso de cache (em teste) e o endpoint de email tambem é para testes

--- src/main/java/com/ruanzerah/skyio/SkyioApplication.java
+++ src/main/java/com/ruanzerah/skyio/SkyioApplication.java
@@ -2,8 +2,10 @@ package com.ruanzerah.skyio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SkyioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SkyioApplication.class, args);
--- src/main/java/com/ruanzerah/skyio/domain/user/UserController.java
+++ src/main/java/com/ruanzerah/skyio/domain/user/UserController.java
@@ -1,6 +1,10 @@
 package com.ruanzerah.skyio.domain.user;
 
+import com.ruanzerah.skyio.domain.email.Email;
+import com.ruanzerah.skyio.domain.email.EmailProducer;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +19,29 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+    private final EmailProducer emailProducer;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserByID(@PathVariable("id") UUID id) {
         return ResponseEntity.ok(userService.getByID(id));
     }
 
+    @PostMapping("/email")
+    public ResponseEntity<Object> getUserByID(@RequestBody Email email) {
+        emailProducer.send(email);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<User> deleteAll() {
+        userService.deleteAll();
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping(value = "all", params = {"page", "size"})
+    @Cacheable("requests")
+    @CacheEvict(value = "requests", allEntries = true)
     public List<User> findPaginate(@RequestParam("page") int page, @RequestParam("size") int size) {
         Pageable pageable = PageRequest.of(page, size);
         return userService.getAll(pageable).getContent();
