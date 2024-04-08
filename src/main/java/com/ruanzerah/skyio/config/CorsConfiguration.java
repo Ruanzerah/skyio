272fe2eac97af16b4ae9dbba0fc62caf7291dab8
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-07 22:29:48 -0300

    Agora o front end irá ser nextJs e não angular

--- src/main/java/com/ruanzerah/skyio/config/CorsConfiguration.java
+++ src/main/java/com/ruanzerah/skyio/config/CorsConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST");
     }
 }
