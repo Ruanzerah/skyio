c461eb621195de871c5d07b2eda3e7d259fc080b
gamerYToff <miguellopesdelmondes@gmail.com>
2024-04-07 22:30:43 -0300

    o logging irá ser baseado em email em um futuro talvez com o name tambem

--- src/main/java/com/ruanzerah/skyio/config/CustomUserDetailsService.java
+++ src/main/java/com/ruanzerah/skyio/config/CustomUserDetailsService.java
@@ -14,7 +14,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        return userRepository.findByName(name).orElseThrow(() -> new UsernameNotFoundException("Algum erro desconhecido aconteceu, não foi possivel encontrar o seu usuario"));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Algum erro desconhecido aconteceu, não foi possivel encontrar o seu usuario"));
     }
 }
